b871779c9c4b5f37d133f30507b58704901007c2
GiselleBarbosa <g88barbosa@gmail.com>
2024-09-05 22:56:47 -0300

    first commit

---
+++ src/test/java/com/giselle/cleanarch/CleanarchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giselle.cleanarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanarchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
