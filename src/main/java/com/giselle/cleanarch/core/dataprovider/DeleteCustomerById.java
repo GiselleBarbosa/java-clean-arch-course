651d6d1c53b50bd454df077f7e1a22e506f9a0d3
GiselleBarbosa <g88barbosa@gmail.com>
2024-09-22 18:52:22 -0300

    feat: implementa o useCase para fazer o delete de customer

---
+++ src/main/java/com/giselle/cleanarch/core/dataprovider/DeleteCustomerById.java
@@ -0,0 +1,6 @@
+package com.giselle.cleanarch.core.dataprovider;
+
+public interface DeleteCustomerById {
+
+    void delete(final String id);
+}
--- src/main/java/com/giselle/cleanarch/core/dataprovider/InsertCustomer.java
+++ src/main/java/com/giselle/cleanarch/core/dataprovider/InsertCustomer.java
@@ -3,5 +3,6 @@ package com.giselle.cleanarch.core.dataprovider;
 import com.giselle.cleanarch.core.domain.Customer;
 
 public interface InsertCustomer {
+
     void insert(Customer customer);
 }
