ba6d1f52ddf59a98b0610a3d14275682b140ce2e
GiselleBarbosa <g88barbosa@gmail.com>
2024-09-22 18:27:38 -0300

    chore: indenta codigo

--- src/main/java/com/giselle/cleanarch/core/usecase/UpdateCustomerUseCase.java
+++ src/main/java/com/giselle/cleanarch/core/usecase/UpdateCustomerUseCase.java
@@ -3,5 +3,6 @@ package com.giselle.cleanarch.core.usecase;
 import com.giselle.cleanarch.core.domain.Customer;
 
 public interface UpdateCustomerUseCase {
-    void update(Customer customer, String ZipCode) {}
+
+    void update(Customer customer, String ZipCode);
 }
