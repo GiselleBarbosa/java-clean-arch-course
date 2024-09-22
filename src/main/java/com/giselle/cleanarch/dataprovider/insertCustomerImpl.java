d22a434c4aaf2beea33ff3562cea2fc0398f6a10
GiselleBarbosa <g88barbosa@gmail.com>
2024-09-22 14:36:13 -0300

    feat: implementa dataProvider de insercao do customer

---
+++ src/main/java/com/giselle/cleanarch/dataprovider/insertCustomerImpl.java
@@ -0,0 +1,24 @@
+package com.giselle.cleanarch.dataprovider;
+
+import com.giselle.cleanarch.core.dataprovider.InsertCustomer;
+import com.giselle.cleanarch.core.domain.Customer;
+import com.giselle.cleanarch.dataprovider.repository.CustomerRepository;
+import com.giselle.cleanarch.dataprovider.repository.mapper.CustomerEntityMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class insertCustomerImpl implements InsertCustomer {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerEntityMapper customerEntityMapper;
+
+    @Override
+    public void insert(Customer customer) {
+        var customerEntity = this.customerEntityMapper.toCustomerEntity(customer);
+        customerRepository.save(customerEntity);
+    }
+}
---
+++ src/main/java/com/giselle/cleanarch/dataprovider/repository/mapper/CustomerEntityMapper.java
@@ -0,0 +1,11 @@
+package com.giselle.cleanarch.dataprovider.repository.mapper;
+
+import com.giselle.cleanarch.core.domain.Customer;
+import com.giselle.cleanarch.dataprovider.repository.entity.CustomerEntity;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CustomerEntityMapper {
+
+    CustomerEntity toCustomerEntity(Customer customer);
+}
